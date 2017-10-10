0f10b918297686fedf0cd7bd9a08c993f6f19b67
Silvio Messina <smessina@SM-MacBook.local>
2017-10-09 22:26:29 -0300

    Ignition in 3... 2... 1...

---
+++ more-settings.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP More Settings
+Plugin URI: 
+Description: Extra configurations for Wordpress
+Author: Silvio Messina
+Version: 1.0
+Author URI: http://silviomessina.com/
+*/
+
+
+?>
