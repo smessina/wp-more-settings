c3fbc10cdb9ae58450842711397ab342074b0c86
= <info@silviomessina.com>
2018-01-11 22:04:31 -0300

    Agregado removedor de query string

--- more-settings.php
+++ more-settings.php
@@ -1,12 +1,16 @@
 <?php
 /*
-Plugin Name: WP More Settings
-Plugin URI: https://github.com/smessina/wp-more-settings
-Description: Extra configurations for Wordpress
-Author: Silvio Messina
-Version: 1.0
-Author URI: http://silviomessina.com/
+	Plugin Name: WP More Settings
+	Plugin URI: https://github.com/smessina/wp-more-settings
+	Description: Extra configurations for Wordpress
+	Author: Silvio Messina
+	Version: 1.0
+	Author URI: http://silviomessina.com/
 */
+
+/* Desactivar <p> y <br> */
+//remove_filter('the_content', 'wpautop');
+//remove_filter('the_excerpt', 'wpautop');
 
 function desactivar_comentarios(){
 	return false;
@@ -16,23 +20,27 @@ add_filter('pings_open', 'desactivar_comentarios', 20, 2);
 
 /* Desactivar Emojis */
 function desactivar_emojicons(){
-  // all actions related to emojis
-  remove_action('admin_print_scripts', 'print_emoji_detection_script');
-  remove_action('admin_print_styles', 'print_emoji_styles');
-  remove_action('wp_head', 'print_emoji_detection_script', 7);
-  remove_action('wp_print_styles', 'print_emoji_styles');
-  remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-  remove_filter('the_content_feed', 'wp_staticize_emoji');
-  remove_filter('comment_text_rss', 'wp_staticize_emoji');
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
 
-  // filter to remove TinyMCE emojis
-  add_filter('tiny_mce_plugins', 'disable_emojicons_tinymce');
+	// filter to remove TinyMCE emojis
+	add_filter('tiny_mce_plugins', 'disable_emojicons_tinymce');
 }
 add_action('init', 'desactivar_emojicons');
 
-/* Desactivar <p> y <br> */
-//remove_filter('the_content', 'wpautop');
-//remove_filter('the_excerpt', 'wpautop');
-
+/* Removee query string de archivos estaticos */
+function remover_cssjs_ver( $src ) {
+	if( strpos( $src, '?ver=' ) )
+		$src = remove_query_arg( 'ver', $src );
+	return $src;
+}
+add_filter('style_loader_src', 'remover_cssjs_ver', 10, 2);
+add_filter('script_loader_src', 'remover_cssjs_ver', 10, 2);
 
 ?>
