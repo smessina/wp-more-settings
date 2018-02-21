7e3d2c37c11431acf253b49d9b9e6f782abc524c
smessina <info@silviomessina.com>
2018-02-21 11:40:59 -0300

    Deshabilitar WP REST API

--- more-settings.php
+++ more-settings.php
@@ -29,12 +29,11 @@ function desactivar_emojicons(){
 	remove_filter('the_content_feed', 'wp_staticize_emoji');
 	remove_filter('comment_text_rss', 'wp_staticize_emoji');
 
-	// filter to remove TinyMCE emojis
 	add_filter('tiny_mce_plugins', 'disable_emojicons_tinymce');
 }
 add_action('init', 'desactivar_emojicons');
 
-/* Removee query string de archivos estaticos */
+/* Remover query string de archivos estaticos */
 function remover_cssjs_ver( $src ) {
 	if( strpos( $src, '?ver=' ) )
 		$src = remove_query_arg( 'ver', $src );
@@ -43,4 +42,15 @@ function remover_cssjs_ver( $src ) {
 add_filter('style_loader_src', 'remover_cssjs_ver', 10, 2);
 add_filter('script_loader_src', 'remover_cssjs_ver', 10, 2);
 
+/* Deshabilitar WP REST API */
+function deshabilitar_rest_api(){
+	remove_action('wp_head', 'wp_oembed_add_discovery_links');
+	remove_action('wp_head', 'rest_output_link_wp_head', 10);
+	remove_action('template_redirect', 'rest_output_link_header', 11);
+	remove_action('xmlrpc_rsd_apis', 'rest_output_rsd');
+	add_filter('rest_authentication_errors', function($access) {
+		return new WP_Error( 'rest_disabled', __( 'The REST API on this site has been disabled.' ), array( 'status' => rest_authorization_required_code() ) );
+	});
+}
+add_action('init', 'deshabilitar_rest_api');
 ?>
